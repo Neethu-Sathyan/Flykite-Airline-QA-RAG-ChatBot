ee49f9404808605107c5d333266af42eed7c614b
Dr Neethu Sathyan M <neethusathyanm@gmail.com>
2026-07-05 21:52:50 +0530

    Update repo_id for uploading to Hugging Face

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -4,7 +4,7 @@ HF_TOKEN = os.environ.get("HF_TOKEN", "")
 api = HfApi(token=HF_TOKEN)
 api.upload_folder(
     folder_path="deployment",     # the local folder containing your files
-    repo_id="Neethu-Sathyan/Flykite-Airline-QA-RAG-ChatBot",          # the target repo
+    repo_id="Neethu-Sathyan/Flykite-Airline-RAG-ChatBot",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
